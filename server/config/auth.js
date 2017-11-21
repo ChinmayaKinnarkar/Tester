fe4e854bb17593955f9978f28f5ce69428d85930
Chinmaya Kinnarkar <kinnarkarchinmaya@gmail.com>
2017-11-21 19:51:45 +0530

    Update auth.js

--- server/config/auth.js
+++ server/config/auth.js
@@ -2,6 +2,6 @@ module.exports = {
 	'facebookAuth' : {
 		'clientID': '805414282916861',
 		'clientSecret': '04eca537d35151ca6bc6833e795e3fc5',
-		'callbackURL': 'http://ec2-54-179-155-238.ap-southeast-1.compute.amazonaws.com:3000/auth/facebook/callback'
+		'callbackURL': 'https://rocky-hollows-68289.herokuapp.com/auth/facebook/callback'
 	}
 }
